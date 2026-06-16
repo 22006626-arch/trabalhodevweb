84017aaef70e5b0e8dd8205aee7424a837a72e55
22006626-arch <22006626@uepg.br>
2026-06-16 17:03:41 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,9 +1,6 @@
-CREATE DATABASE IF NOT EXISTS gemeos_aniversario;
-USE gemeos_aniversario;
-
 CREATE TABLE IF NOT EXISTS gemeos_salvos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     detalhes TEXT,
-    data_descoberta TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    data_descoberta TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
